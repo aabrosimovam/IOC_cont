23a616bee4a8c6179e207f36fc9626562fcd40f9
rita <pocyriteko@gmail.com>
2023-06-19 11:08:09 +0700

    add moodle data
    *добавление данных из мудла

--- iprocessor.h
+++ iprocessor.h
@@ -1,4 +1,35 @@
 #ifndef IPROCESSOR_H
 #define IPROCESSOR_H
 
+enum ProcessorType
+{ x86,
+   x64
+};
+
+class Computer
+{
+   public:
+        IntelProcessor* GetProcessor(double speed, ProcessorType type, string version)
+    {
+       return new IntelProcessor(speed, type, version);
+    }
+};
+
+class IntelProcessor
+{
+     string Version;
+     ProcessorType Type;
+     double Speed;
+   public:
+     IntelProcessor(????){????}
+
+//get,set????
+
+  string GetProcessorInfo()
+  {
+   return "Processor for" + Version + Speed + Type;
+  }
+
+ };
+
 #endif // IPROCESSOR_H
